b46b67c9105461571733abff57aa97a9e215454d
stephanyvargas <yostephy@hotmail.com>
2022-01-23 20:00:02 +0900

    EOF fixed

--- city_weather/lib.py
+++ city_weather/lib.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) $
-
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 def try_me(city=' '):
     city = city+' weather'
     city = city.replace(" ", "+")
     res = requests.get(
-        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j$
+        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
     location = soup.select('#wob_loc')[0].getText().strip()
     time = soup.select('#wob_dts')[0].getText().strip()
